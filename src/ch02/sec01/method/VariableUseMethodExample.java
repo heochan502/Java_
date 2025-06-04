8867b160b68d48da65d01803c42ec94b785bc914
greenhc502 <greenhc502@gmail.com>
2025-06-04 18:05:10 +0900

    0604 8교시끝

--- src/ch02/sec01/method/VariableUseMethodExample.java
+++ src/ch02/sec01/method/VariableUseMethodExample.java
@@ -4,12 +4,30 @@ public class VariableUseMethodExample {
     public static void main (String[] args)
     {
         printTime(9,20);
-        printTime(-3,-0);
+        printTime(0,1);
+
+        printTime(2,9, 2);
+        printTime(0,1, 1);
     }
     public static void printTime(int hour, int min){
-        System.out.println("호출됐다 ㅋㅋㅋㅋㅋ");
+       // System.out.println("호출됐다 ㅋㅋㅋㅋㅋ");
        // System.out.printf("%02d : %02d\n" ,hour,min);
+        //this(hour, min);
+      // printTime(hour,min);
+       // System.out.println( ((hour<10 && hour>=0)? "0"+hour : hour) + " : "+ ((min<10 && min>=0)? "0"+min : min));
+        System.out.println(parseTime(hour) +" : "+ parseTime(min));
+    }
+    public static void printTime(int hour, int min, int sec){
+       // System.out.println("호출됐다 ㅋㅋㅋㅋㅋ");
+        // System.out.printf("%02d : %02d\n" ,hour,min);
+        //System.out.print( ((hour<10 && hour>=0)? "0"+hour : hour)+" : ");
+        System.out.print( parseTime(hour) + " : ");
+        printTime(min,sec);
+       // System.out.println( ((hour<10 && hour>=0)? "0"+hour : hour) + " : "+ ((min<10 && min>=0)? "0"+min : min)+ " : "+ ((sec<10 && sec>=0)? "0"+sec : sec));
+    }
 
-        System.out.println( ((hour<10 && hour>=0)? "0"+hour : hour) + " : "+ ((min<10 && min>=0)? "0"+min : min));
+    public static String parseTime(int val)
+    {
+        return val <10 ?"0"+val : String.valueOf(val);
     }
 }
