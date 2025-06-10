d6dbdd7b871c78bb47e55f1f6565f3614159981e
greenhc502 <greenhc502@gmail.com>
2025-06-10 16:07:41 +0900

    0610 6교시중간

---
+++ src/ch06/sec08/exam02/Computer.java
@@ -0,0 +1,8 @@
+package ch06.sec08.exam02;
+
+public class Computer {
+    public int sum(int... values)
+    {
+        return 0;
+    }
+}
---
+++ src/ch06/sec08/exam02/ComputerExample.java
@@ -0,0 +1,10 @@
+package ch06.sec08.exam02;
+
+public class ComputerExample {
+    public static void main(String[] args) {
+        Computer com1 = new Computer();
+        com1.sum(1,2,3);
+        com1.sum(1,2,3,4,5);
+        com1.sum(1,2,3,4,5,6,7,8);
+    }
+}
